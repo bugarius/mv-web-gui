cf3a1fae57a5ce9c0c672e5a9787189892c00fa7
Mariusz Bugara <mariusz.bugara@gmail.com>
2021-02-03 16:46:13 +0100

    updated page name, logo and favicon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -55,12 +55,10 @@ class Header extends Component {
                     <div className="navbar-header">
                         <a className="navbar-brand" href="#/">
                             <div className="brand-logo text-gray">
-                                {/*<img className="img-fluid" src="img/logo.png" alt="App Logo" />*/}
-                                Moja Winnica
+                                <img className="img-fluid" src="img/logo.png" alt="App Logo" />
                             </div>
                             <div className="brand-logo-collapsed text-gray">
-                                MV
-                                {/*<img className="img-fluid" src="img/logo-single.png" alt="App Logo" />*/}
+                                <img className="img-fluid" src="img/logo-single.png" alt="App Logo" />
                             </div>
                         </a>
                     </div>
